f4afe6407b6b22c696e675cde71255dc85f28ef1
deeprakhasiya <deeprakhasiya15@gmail.com>
2025-01-21 17:05:00 +0530

    Adding first endpoint

---
+++ WebApplicationAPI/Controllers/VillaApiController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationAPI.Models;
+
+namespace WebApplicationAPI.Controllers
+{
+    [Route("/api/VillaAPI")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas() {
+            return new List<Villa> {
+                new Villa { Id = 1,Name = "Villa1" } ,
+                new Villa { Id = 2, Name = "Villa2" }
+            }; 
+            }
+    }
+}
---
+++ WebApplicationAPI/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplicationAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
